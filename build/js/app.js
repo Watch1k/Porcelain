a9081aacb03ce6fde1032cb30b85477e34d0a6c6
Vitali Shapovalov <shapovalov.vitali.v@gmail.com>
2017-01-18 03:35:20 +0200

    update

--- build/js/app.js
+++ build/js/app.js
@@ -1735,7 +1735,8 @@ $(function() {
       {
         breakpoint: 768,
         settings: {
-          slidesToShow: 1
+          slidesToShow: 1,
+          variableWidth: false
         }
       }
     ]
--- src/js/lib/open-doctor.js
+++ src/js/lib/open-doctor.js
@@ -8,7 +8,8 @@ $(function() {
       {
         breakpoint: 768,
         settings: {
-          slidesToShow: 1
+          slidesToShow: 1,
+          variableWidth: false
         }
       }
     ]
